39a86dfa5e69661806de3a78a0693a819ea3d49a
jmedina28 <juanperezmedina67@gmail.com>
2022-12-20 21:39:58 +0100

    añado mis pruebas en el script calculos

---
+++ ejercicios/calculos.py
@@ -0,0 +1,8 @@
+from operaciones import suma, resta, producto, division
+
+a, b, c, d = (10, 5, 0, "Hola")
+
+print( "{} + {} = {}".format(a, b, suma(a, b) ) )
+print( "{} - {} = {}".format(b, d, resta(b, d) ) )
+print( "{} * {} = {}".format(b, b, producto(b, b) ) )
+print( "{} / {} = {}".format(a, c, division(a, c) ) )
